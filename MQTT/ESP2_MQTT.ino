bc08921228318c67d96bd96d27b3bcabdd632d76
UtkarshSarbahiTecholution <140433003+UtkarshSarbahiTecholution@users.noreply.github.com>
2024-12-10 14:10:38 -0500

    Add files via upload

--- MQTT/ESP2_MQTT.ino
+++ MQTT/ESP2_MQTT.ino
@@ -20,7 +20,7 @@
 #define S3_MUX3 26
 #define COM_OUT_MUX3 34
 
-#define FSR_THRESHOLD 1000
+#define FSR_THRESHOLD 2000
 
 const char* ssid = "Techolution";
 const char* password = "techolutionisthebe$t";
@@ -30,9 +30,18 @@ const int mqtt_port = 1820;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+const int link3_left[] = { 3, 0, 3, 1, 3, 2, 3, 3, 3, 4, 3, 5, 3, 6, 3, 7, 3, 8 };
+const int link2_left[] = { 2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 2, 5 };
+const int link1_left[] = { 3, 9, 3, 11 };
+
+const int link3_right[] = { 1, 4, 1, 8, 1, 3, 1, 9, 1, 10, 1, 5, 1, 6, 1, 11, 1, 7 };
+const int link2_right[] = { 2, 6, 2, 7, 2, 8, 2, 11, 2, 9, 2, 10 };
+const int link1_right[] = { 1, 0, 1, 2 };
+
 void setupWiFi();
 void reconnect();
-void publishFSRValues(int link, const char* side, JsonDocument& doc);
+void handleFSR(const char* linkName, const char* side, const int* group, int size);
+void publishFSRValues(const char* linkName, const char* side, JsonDocument& doc);
 void selectMuxChannel(int mux, int channel);
 int readMux(int mux, int channel);
 
@@ -48,15 +57,15 @@ void setup()
   pinMode(S3_MUX1, OUTPUT);
   pinMode(COM_OUT_MUX1, INPUT);
 
-  pinMode(S0_MUX2, OUTPUT); 
+  pinMode(S0_MUX2, OUTPUT);
   pinMode(S1_MUX2, OUTPUT);
-  pinMode(S2_MUX2, OUTPUT); 
+  pinMode(S2_MUX2, OUTPUT);
   pinMode(S3_MUX2, OUTPUT);
   pinMode(COM_OUT_MUX2, INPUT);
 
-  pinMode(S0_MUX3, OUTPUT); 
+  pinMode(S0_MUX3, OUTPUT);
   pinMode(S1_MUX3, OUTPUT);
-  pinMode(S2_MUX3, OUTPUT); 
+  pinMode(S2_MUX3, OUTPUT);
   pinMode(S3_MUX3, OUTPUT);
   pinMode(COM_OUT_MUX3, INPUT);
 
@@ -71,25 +80,28 @@ void loop()
   }
   client.loop();
 
-  handleFSR(1, "Left", 0, 9, 1);
-  handleFSR(1, "Right", 9, 18, 1); 
-  handleFSR(2, "Left", 0, 6, 2); 
-  handleFSR(2, "Right", 6, 12, 2);
-  handleFSR(3, "Left", 0, 3, 3);    
-  handleFSR(3, "Right", 3, 6, 3);   
+  handleFSR("3", "left", link3_left, sizeof(link3_left) / sizeof(link3_left[0]));
+  handleFSR("2", "left", link2_left, sizeof(link2_left) / sizeof(link2_left[0]));
+  handleFSR("1", "left", link1_left, sizeof(link1_left) / sizeof(link1_left[0]));
+
+  handleFSR("3", "right", link3_right, sizeof(link3_right) / sizeof(link3_right[0]));
+  handleFSR("2", "right", link2_right, sizeof(link2_right) / sizeof(link2_right[0]));
+  handleFSR("1", "right", link1_right, sizeof(link1_right) / sizeof(link1_right[0]));
 
   delay(500);
 }
 
-void handleFSR(int link, const char* side, int startChannel, int endChannel, int mux) 
+void handleFSR(const char* linkName, const char* side, const int* group, int size) 
 {
   StaticJsonDocument<512> doc;
   JsonArray fsrValues = doc.createNestedArray("fsr_values");
 
   bool sendData = false;
 
-  for (int channel = startChannel; channel < endChannel; channel++) 
+  for (int i = 0; i < size; i += 2) 
   {
+    int mux = group[i];
+    int channel = group[i + 1];
     int fsrValue = readMux(mux, channel);
 
     if (fsrValue > FSR_THRESHOLD) 
@@ -99,25 +111,25 @@ void handleFSR(int link, const char* side, int startChannel, int endChannel, int
     } 
     else 
     {
-      fsrValues.add(0); 
+      fsrValues.add(0);
     }
   }
 
   if (sendData) 
   {
-    publishFSRValues(link, side, doc);
+    publishFSRValues(linkName, side, doc);
   }
 }
 
-void publishFSRValues(int link, const char* side, JsonDocument& doc) 
+void publishFSRValues(const char* linkName, const char* side, JsonDocument& doc) 
 {
-  doc["link"] = link;
+  doc["link"] = linkName;
   doc["side"] = side;
 
   char buffer[512];
   serializeJson(doc, buffer);
 
-  String topic = "fsr/link" + String(link) + "/" + side;
+  String topic = "fsr/link" + String(linkName) + "/" + side;
   client.publish(topic.c_str(), buffer);
 
   Serial.printf("Published to %s: %s\n", topic.c_str(), buffer);
@@ -125,26 +137,26 @@ void publishFSRValues(int link, const char* side, JsonDocument& doc)
 
 void selectMuxChannel(int mux, int channel) 
 {
-  if (mux == 1) 
+  switch (mux) 
   {
-    digitalWrite(S0_MUX1, channel & 0x01);
-    digitalWrite(S1_MUX1, (channel >> 1) & 0x01);
-    digitalWrite(S2_MUX1, (channel >> 2) & 0x01);
-    digitalWrite(S3_MUX1, (channel >> 3) & 0x01);
-  } 
-  else if (mux == 2) 
-  {
-    digitalWrite(S0_MUX2, channel & 0x01);
-    digitalWrite(S1_MUX2, (channel >> 1) & 0x01);
-    digitalWrite(S2_MUX2, (channel >> 2) & 0x01);
-    digitalWrite(S3_MUX2, (channel >> 3) & 0x01);
-  } 
-  else if (mux == 3) 
-  {
-    digitalWrite(S0_MUX3, channel & 0x01);
-    digitalWrite(S1_MUX3, (channel >> 1) & 0x01);
-    digitalWrite(S2_MUX3, (channel >> 2) & 0x01);
-    digitalWrite(S3_MUX3, (channel >> 3) & 0x01);
+    case 1:
+      digitalWrite(S0_MUX1, channel & 0x01);
+      digitalWrite(S1_MUX1, (channel >> 1) & 0x01);
+      digitalWrite(S2_MUX1, (channel >> 2) & 0x01);
+      digitalWrite(S3_MUX1, (channel >> 3) & 0x01);
+      break;
+    case 2:
+      digitalWrite(S0_MUX2, channel & 0x01);
+      digitalWrite(S1_MUX2, (channel >> 1) & 0x01);
+      digitalWrite(S2_MUX2, (channel >> 2) & 0x01);
+      digitalWrite(S3_MUX2, (channel >> 3) & 0x01);
+      break;
+    case 3:
+      digitalWrite(S0_MUX3, channel & 0x01);
+      digitalWrite(S1_MUX3, (channel >> 1) & 0x01);
+      digitalWrite(S2_MUX3, (channel >> 2) & 0x01);
+      digitalWrite(S3_MUX3, (channel >> 3) & 0x01);
+      break;
   }
 }
 
@@ -187,8 +199,7 @@ void reconnect()
     if (client.connect("ESP32_FSR_Client")) 
     {
       Serial.println("Connected to MQTT broker.");
-    } 
-    else 
+    } else 
     {
       Serial.printf("Failed (rc=%d). Retrying in 5 seconds...\n", client.state());
       delay(5000);
